7a37680e6fb0b746b479e9861511820d6a269815
Simran <s.singh.xyz@gmail.com>
2023-09-11 00:34:49 -0400

    metros

--- data.js
+++ data.js
@@ -71,6 +71,78 @@ export const metroData = {
       'Blue,Orange,Red',
       'https://www.google.com/maps/dir//Metro+Center,+607+13th+St+NW,+Washington,+DC+20005/@38.8918502,-77.0236405,15.55z',
       '1'
+    ],
+     [
+      '2',
+      'Federal Triangle',
+      '38.893906',
+      '-77.028475',
+      'Blue,Orange',
+      'https://www.google.com/maps/dir//Federal+Triangle,+12th+Street+Northwest,+Washington,+DC/@38.8938331,-77.0695188,13z',
+      '1'
+    ],
+    [
+      '3',
+      'Smithsonian',
+      '38.888934',
+      '-77.02845',
+      'Blue,Orange',
+      'https://www.google.com/maps/dir//Smithsonian,+Washington,+DC/@38.8891381,-77.0695696,13z',
+      '1'
+    ],
+    [
+      '4',
+      'L Enfant',
+      '38.885721',
+      '-77.021765',
+      'Blue,Orange,Green,Yellow',
+      "https://www.google.com/maps/dir//L'Enfant+Plaza,+600+Maryland+Ave+SW,+Washington,+DC+20024/@38.889839,-77.0235895,14.88z",
+      '1'
+    ],
+    [
+      '5',
+      'Archives-Navy Memorial-Penn Quarter',
+      '38.893844',
+      '-77.022146',
+      'Green,Yellow',
+      "https://www.google.com/maps/dir//Archives-Navy+Memorial-Penn+Quarter,+Washington,+DC+20004/@38.8939594,-77.0634234,13z",
+      '1'
+    ],
+    [
+      '6',
+      'McPherson Square Metrorail Station',
+      '38.9006088',
+      '-77.0314252',
+      'Blue,Orange,Silver',
+      "https://www.google.com/maps/dir//Gallery+Pl-Chinatown,+630+H+St+NW,+Washington,+DC+20001/@38.8985602,-77.0239662,17z",
+      '1'
+    ],
+    [
+      '7',
+      'Gallery PIace Chinatown',
+      '38.899642',
+      '-77.021843',
+      'Green,Red,Yellow',
+      "https://www.google.com/maps/dir//Gallery+Pl-Chinatown,+630+H+St+NW,+Washington,+DC+20001/@38.8985602,-77.0239662,17z",
+      '1'
+    ],
+    [
+      '8',
+      'Farragut North',
+      '38.9017356',
+      '-77.039578',
+      'Red',
+      "https://www.google.com/maps/dir//Farragut+North,+1001+Connecticut+Ave+NW,+Washington,+DC+20036/@38.8957873,-77.0411774,15.42z",
+      '1'
+    ],
+    [
+      '9',
+      'Farragut West',
+      '38.9010474',
+      '-77.0422439',
+      'Blue,Orange,Silver',
+      "https://www.google.com/maps/dir//Gallery+Pl-Chinatown,+630+H+St+NW,+Washington,+DC+20001/@38.8985602,-77.0239662,17z",
+      '1'
     ]
   ]
 };
